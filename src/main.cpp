2546d97dd3c232a914c6cf4073863740e635edab
Graham Brooks <graham@grahambrooks.com>
2018-11-27 18:51:38 -0700

    Removed secret token - thanks @github for catching and removing. Fortunately it had restricted access but good to know that it was disabled immediately.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
 #include "GitHub.h"
+#include "CommandLineParser.h"
 
 int main(int argc, char *argv[]) {
 
-    GitHub github;
+    CommandLineParser parser;
 
-    github.repos("840186a212922b9b1ebb9bb63546151743d076c8");
+    auto arguments = parser.parse(argc, argv);
 
+    if (arguments.valid()) {
+        GitHub github;
+
+        github.repos("");
+    } else {
+        std::cout << arguments.error_message();
+    }
     return 0;
 }
